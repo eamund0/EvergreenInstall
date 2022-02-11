405500d5d5d3e9108b88e1e8930638652f6f23ea
eamund0 <97538099+eamund0@users.noreply.github.com>
2022-02-11 16:07:19 +0000

    Update VisualStudioCode.ps1

--- VisualStudioCode.ps1
+++ VisualStudioCode.ps1
@@ -6,5 +6,5 @@ if (-not(Test-Path $BuildDir)){
 $VSCode = Get-EvergreenApp -Name MicrosoftVisualStudioCode  | Where-Object { $_.channel -eq "Stable" -and $_.Architecture -eq "x64" -and $_.Platform -eq "win32-x64"}
 $VSCInstaller = Split-Path -Path $VSCode.Uri -Leaf
 Invoke-WebRequest -Uri $VSCode.Uri -OutFile "c:\buildactions\$VSCInstaller" -UseBasicParsing
-& "c:\buildactions\$VSCInstaller" "/verysilent /norestart"
+& "c:\buildactions\$VSCInstaller "/verysilent" "/suppressmsgboxes" "/MERGETASKS=!runcode"
 Write-Output 'VSCode Installed'
