ed2b239d2442c756c82cd9f4ed9565ef7ed7ab6d
eamund0 <97538099+eamund0@users.noreply.github.com>
2022-02-11 15:12:14 +0000

    Update M365V1.ps1

--- M365V1.ps1
+++ M365V1.ps1
@@ -1,5 +1,6 @@
 $M365 = Get-EvergreenApp -Name Microsoft365apps  | Where-Object { $_.channel -eq "semiannual"}
 $M365Installer = Split-Path -Path $M365.Uri -Leaf
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/eamund0/EvergreenInstall/main/v1configuration.xml" -OutFile "C:\buildactions\v1configuration.xml"
 Invoke-WebRequest -Uri $M365.uri -OutFile "c:\apps\$M365Installer" -UseBasicParsing
-& ".\$M365Installer" "/download" "c:\apps\v1configuration.xml"
-& ".\$M365Installer" "/configure" "c:\apps\v1configuration.xml"
+& ".\$M365Installer" "/download" "c:\buildactions\v1configuration.xml"
+& ".\$M365Installer" "/configure" "c:\buildactions\v1configuration.xml"
